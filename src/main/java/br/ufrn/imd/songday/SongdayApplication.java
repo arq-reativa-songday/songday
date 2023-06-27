ded11d3e7568a844dfad062b99458746db268a20
Márcio Tenório <marciotenorioj@gmail.com>
2023-06-27 15:03:32 -0300

    register in discovery and have route in gateway

--- src/main/java/br/ufrn/imd/songday/SongdayApplication.java
+++ src/main/java/br/ufrn/imd/songday/SongdayApplication.java
@@ -2,14 +2,24 @@ package br.ufrn.imd.songday;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
 public class SongdayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SongdayApplication.class, args);
 	}
 
+	@LoadBalanced
+	@Bean
+	public RestTemplate getRestTemplate(){
+		return new RestTemplate();
+	}
 }
--- src/main/java/br/ufrn/imd/songday/client/SongsClient.java
+++ src/main/java/br/ufrn/imd/songday/client/SongsClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "songs-service", url = "${songs.api.address}")
+@FeignClient(name = "songs")
 public interface SongsClient {
     @GetMapping(value = "/songs/{id}")
     ResponseEntity<Object> findById(@PathVariable String id);
